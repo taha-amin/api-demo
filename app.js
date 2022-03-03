031636a789f8ad02753b8c8cb55f88cb9e2168e3
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-03 11:57:15 -0800

    added function that will load pokedex with pokemon names and type

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 // import functions
 
+import { getPokedex } from "./fetch";
+
 // grab DOM elements
 const template = document.querySelector('#template');
 const selectEl = document.querySelector('select');
 const listEl = document.querySelector('#list');
 
+async function loadPokedex() {
+    const pokedex = await getPokedex();
 
-// set event listeners 
+    listEl.classList.add('pokemon');
+
+    for (let pokemon of pokedex) {
+        const clone = template.content.cloneNode(true);
+
+        const name = clone.querySelector('h2');
+        const image = clone.querySelector('img');
+        const type = clone.querySelector('h6');
+
+        name.textContent = 'Name :' + pokemon.pokemon;
+
+        type.textContent = 'Egg :' + pokemon.egg_group_1;
+        image.src = pokemon.url_image;
+        image.alt = pokemon.pokedex;
+
+        listEl.appendChild(clone);
+    }
+}
+
+// set event listeners
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
