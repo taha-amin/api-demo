467e691f9e4e61bdec27e7e0f4aeb2cdaff818ab
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-03 11:36:00 -0800

    created fetch.js file with export function

---
+++ fetch.js
@@ -0,0 +1,6 @@
+export async function getPokedex() {
+    let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+    const resp = await fetch(url);
+    const json = await.resp.json();
+    return json.results;
+}
